03a6835bc330b9005814710374f491a949055c0f
fatma20010 <fatmahammedi51@gmail.com>
2024-07-03 13:17:06 +0100

    the final version of my project

---
+++ main.py
@@ -0,0 +1,48 @@
+import joblib
+from flask import Flask,render_template,request
+app = Flask(__name__)
+try:
+  with open('decisionTree.joblib', 'rb') as f:
+    model = joblib.load(f)
+  print("Model loaded successfully!")
+except FileNotFoundError:
+  print("Model file not found!")
+@app.route('/')
+def GO():
+    return render_template('GO.html')
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    # Get individual values from the form
+    ra = request.form.get('ra')
+    dec = request.form.get('dec')
+    u = request.form.get('u')
+    g = request.form.get('g')
+    r = request.form.get('r')
+    i = request.form.get('i')
+    z = request.form.get('z')
+
+    # Combine values into a list 
+    input_data = [[ra, dec, u, g, r, i, z]]
+
+    # Make prediction
+    prediction = model.predict(input_data)
+    output=prediction
+    return render_template('GO.html',prediction_text=f'your celestial object is {output}')
+
+
+                           
+if __name__ == '__main__':
+  app.run(debug=True)
+                       
+
+
+
+
+
+
+
+
+
+
+
+
